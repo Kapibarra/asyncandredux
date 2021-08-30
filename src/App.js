cdc870faea5880d35de86bfe472e2f025b224b27
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-30 14:01:31 +0300

    added stucture

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { useEffect } from 'react'
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  useEffect ( () => {
-    axios.get('http://api.weatherapi.com/v1/current.json?key=34c281611c34485b8e6101749213008&q=moscow&aqi=no').then( data => {
-      console.log(data);
-    })
-  },[])
+  const [weather, setWeather] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        "http://api.weatherapi.com/v1/current.json?key=34c281611c34485b8e6101749213008&q=moscow&aqi=no"
+      )
+      .then((data) => {
+        console.log(data.data)
+        setWeather(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
-
+      {weather && (
+        <div>
+          <h1>Погода в {weather.location.name}</h1>
+          <h2>{weather.location.region}</h2>
+          <div className="condition">
+            <h3>{weather.current.condition.text}</h3>
+            <img src={weather.current.condition.icon}></img>
+            <h4>Температура : {weather.current.temp_c}</h4>
+            <h4>Ощущается как : {weather.current.feelslike_c}</h4>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

0cf55cce2a88c9fe1a2274702d0734b07b3c90f1
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-08-30 13:42:14 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import React, { useEffect } from 'react'
 import './App.css';
 import axios from 'axios';
 
 function App() {
+  useEffect ( () => {
+    axios.get('http://api.weatherapi.com/v1/current.json?key=34c281611c34485b8e6101749213008&q=moscow&aqi=no').then( data => {
+      console.log(data);
+    })
+  },[])
   return (
     <div className="App">
 
